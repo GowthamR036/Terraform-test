d443e812d9b0a0d671ddea5f5c936ea6904ad308
Prafull Chavan <99485204+prafullchvn@users.noreply.github.com>
2023-03-14 19:25:16 +0530

    Implemented terraform api using nodejs and express.

--- nodejs/app.ts
+++ nodejs/app.ts
@@ -1,6 +1,77 @@
 import { Terraform } from './terraform';
+import express from 'express';
+
+const app = express();
 
 const tf = new Terraform('../node/resource_group');
 
-const initOuput = tf.init();
-console.log(initOuput);
+type ResourceGroup = {
+  name: string;
+  location: string;
+};
+
+app.use(express.json());
+
+app.use('/resource_group', (req, res, next) => {
+  const clientId = req.query.client_id;
+  const clientSecret = req.query.client_secret;
+  const subscriptionId = req.query.subscription_id;
+  const tenantId = req.query.tenant_id;
+
+  if (!(clientId && clientSecret && subscriptionId && tenantId)) {
+    return res.status(405).send('Query params missing.');
+  }
+
+  process.env['ARM_CLIENT_ID'] = clientId;
+  process.env['ARM_CLIENT_SECRET'] = clientSecret;
+  process.env['ARM_SUBSCRIPTION_ID'] = subscriptionId;
+  process.env['ARM_TENANT_ID'] = tenantId;
+
+  const rg: ResourceGroup = req.body as ResourceGroup;
+  console.log(!(rg.name && rg.location));
+  if (!(rg.name && rg.location)) {
+    return res.status(405).send('Request body is missing.');
+  }
+
+  req.rg = rg;
+  next();
+});
+
+app.use('/resource_group', (req, res, next) => {
+  const initOutput = tf.init();
+
+  if (!initOutput) {
+    return res.status(500).send('Init failed');
+  }
+
+  next();
+});
+
+app.post('/resource_group', (req, res) => {
+  const planOutput = tf.plan(req.rg);
+  if (!planOutput) {
+    return res.status(500).send('Plan failed');
+  }
+  const applyOutput = tf.apply(req.rg);
+  if (!applyOutput) {
+    return res.status(500).send('Apply failed');
+  }
+
+  res.status(200).send('Resource created successfuly.');
+});
+
+app.delete('/resource_group', (req, res) => {
+  const destroyOutput = tf.destroy(req.rg);
+  if (!destroyOutput) {
+    return res.status(500).send('Destroy failed');
+  }
+
+  res.status(200).send('Resource destroyed successfuly.');
+});
+
+const PORT = 8080;
+app.listen(PORT, () =>
+  console.log(
+    `Server started at port ${PORT}. http://localhost:${PORT}/resource_group`
+  )
+);
--- nodejs/terraform.ts
+++ nodejs/terraform.ts
@@ -5,6 +5,55 @@ interface KeyValuePair {
   [index: string]: string;
 }
 
+class TerraformCommand {
+  options: [string, string, boolean][];
+  variables: [string, string][];
+  command: string;
+  terraformBinPath: string;
+
+  constructor(command: string, terraformBinPath?: string) {
+    this.command = command;
+    this.terraformBinPath = terraformBinPath || 'terraform';
+    this.variables = [];
+    this.options = [];
+  }
+
+  addOption({
+    option,
+    value,
+    isFlagged,
+  }: {
+    option: string;
+    value?: string;
+    isFlagged?: boolean;
+  }) {
+    const optionArr: any = [option, value, isFlagged || false];
+    this.options.push(optionArr);
+  }
+
+  addVariable(varName: string, value: string) {
+    this.variables.push([varName, value]);
+  }
+
+  build() {
+    let cmd = `${this.terraformBinPath} ${this.command} `;
+
+    this.options.forEach(([option, value, isFlagged]) => {
+      if (isFlagged) {
+        cmd += ` -${option}`;
+      }
+      cmd += ` -${option}=${value}`;
+    });
+
+    this.variables.forEach(([option, value]) => {
+      cmd += ` -var ${option}=${value}`;
+    });
+    console.log('command is ', cmd);
+
+    return cmd;
+  }
+}
+
 export class Terraform {
   workingDir: string;
   terraformBinPath: string;
@@ -17,13 +66,13 @@ export class Terraform {
 
   constructor(
     workingDir: string,
+    terraformBinPath?: string,
+    var_file?: string,
     targets?: Set<string>,
     state?: string,
     variables?: KeyValuePair,
     parallelism?: string,
-    var_file?: string,
-    isEnvVarsIncluded?: boolean,
-    terraformBinPath?: string
+    isEnvVarsIncluded?: boolean
   ) {
     this.workingDir = workingDir;
     this.state = state;
@@ -40,6 +89,73 @@ export class Terraform {
     cmd += backend ? ` -backend=${backend}` : '';
     cmd += backendConfig ? ` -backend-config=${backendConfig}` : '';
     cmd += reconfigure ? ` -reconfigure` : '';
+
+    try {
+      const output = execSync(cmd, { cwd: this.workingDir });
+      console.log('Output:', output.toString());
+    } catch (err) {
+      console.error('Error:', err);
+      return false;
+    }
+
+    return true;
+  }
+
+  plan(variables: KeyValuePair) {
+    let cmd: string = `${this.terraformBinPath} plan -input=false`;
+    cmd += this.var_file ? ` -var-file=${this.var_file}` : '';
+
+    let planVariables = Object.entries(variables).reduce(
+      (str, [key, value]) => (str += ` -var ${key}=${value}`),
+      ''
+    );
+    cmd += planVariables;
+
+    try {
+      const output = execSync(cmd, { cwd: this.workingDir });
+      console.log('Output:', output.toString());
+    } catch (err) {
+      console.error('Error:', err);
+      return false;
+    }
+
+    return true;
+  }
+
+  apply(variables: KeyValuePair, autoApprove: boolean = true) {
+    let cmd: string = `${this.terraformBinPath} apply -input=false`;
+    cmd += this.var_file ? ` -var-file=${this.var_file}` : '';
+    cmd += autoApprove ? ` -auto-approve` : '';
+
+    let planVariables = Object.entries(variables).reduce(
+      (str, [key, value]) => (str += ` -var ${key}=${value}`),
+      ''
+    );
+
+    cmd += planVariables;
+
+    try {
+      const output = execSync(cmd, { cwd: this.workingDir });
+      console.log('Output:', output.toString());
+    } catch (err) {
+      console.error('Error:', err);
+      return false;
+    }
+
+    return true;
+  }
+
+  destroy(variables: KeyValuePair, autoApprove: boolean = true) {
+    let cmd: string = `${this.terraformBinPath} destroy -input=false`;
+    cmd += this.var_file ? ` -var-file=${this.var_file}` : '';
+    cmd += autoApprove ? ` -auto-approve` : '';
+
+    let planVariables = Object.entries(variables).reduce(
+      (str, [key, value]) => (str += ` -var ${key}=${value}`),
+      ''
+    );
+
+    cmd += planVariables;
 
     try {
       const output = execSync(cmd, { cwd: this.workingDir });
